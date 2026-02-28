98029848eae0f3f07aeaa70fd974efa066e3d948
AayushTrivedi3103 <134122495+AayushTrivedi3103@users.noreply.github.com>
2026-02-28 23:12:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post("/api/edit-profile", async (req, res) => {
         } = req.body;
 
         const response = await axios.post(
-            "https://postkiyaapp.shivanshastrology.in/newproject/api/auth/edit-profile",
+            "https://postkiyaapp.shivanshastrology.in/newproject/api/auth/edit-profile.php",
             {
                 id: id,
                 first_name: first_name,
@@ -168,6 +168,7 @@ app.listen(5000, "0.0.0.0", () => {
     console.log("Server running");
 
 });
+
 
 
 
